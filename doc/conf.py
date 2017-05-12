57fdfb04e68ec54b5b7de177b49624321c9e251f
Yasuhiro Naito <ynaito@sfc.keio.ac.jp>
2017-05-12 14:14:08 +0900

    Version 3.2.4c

--- doc/conf.py
+++ doc/conf.py
@@ -11,8 +11,8 @@ master_doc = 'index'
 project = 'ecell3'
 copyright = '2017, E-Cell project'
 
-version = '3.2.4b'
-release = '3.2.4b'
+version = '3.2.4c'
+release = '3.2.4c'
 
 exclude_trees = ['_build']
 
